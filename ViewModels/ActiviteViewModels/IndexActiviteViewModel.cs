48e268276494a175907309aef18fa519fb698863
jlurel <joseph.lurel@gmail.com>
2018-12-05 22:22:12 -0500

    Update script CreateTable.sql

---
+++ ViewModels/ActiviteViewModels/IndexActiviteViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Garderie.ViewModels.ActiviteViewModels
+{
+    public class IndexActiviteViewModel
+    {
+        public IndexActiviteViewModel()
+        {
+        }
+    }
+}
---
+++ ViewModels/ArticleViewModels/IndexArticleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garderie.ViewModels.ActiviteViewModels
+{
+    public class IndexActiviteViewModel
+    {
+        public int ArticleId { get; set; }
+        public string Nom { get; set; }
+        [Display(Name = "Quantité")]
+        public int Quantite { get; set; }
+        public string Photo { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Catégorie")]
+        public string Categorie { get; set; }
+    }
+}
